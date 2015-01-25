a885e8d42483f99f6e744b06ad0d65e7a8fd5b36
qorpus <dave@qorp.net>
2015-01-25 17:02:09 -0500

    added caching code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## Creates a cache object for the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  mtx <- NULL
+  set <- function(y) {
+    x <<- y
+    mtx <<- NULL
+  }
+  get <- function() return(x)
+  setcache <- function(cache) mtx <<- cache
+  getcache <- function() return(mtx)
+  list(set = set, get = get, setcache = setcache, getcache = getcache ) 
 }
 
-
-## Write a short comment describing this function
-
+## solves the inverse of the matrix, caches the answer in the environment for fast retrieval later
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  mtx <- x$getcache()
+  if(!is.null(mtx)) {
+    message("getting cached data")
+    return(mtx)
+  }
+  data <- x$get()
+  mtx <- solve(data)
+  x$setcache(mtx)
+  mtx
+
 }
